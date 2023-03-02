790273fba655a48d354fb6f996dba39c67ed576d
Mankar123sayu <sayumankar77@gmail.com>
2023-03-02 19:45:15 +0530

    [sayunkta] Add:addition

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Addition_of_two_numbers
+{
+    class Program //entry point of application
+    {
+        static void Main(string[] args)
+        {
+            int x = 15;
+            int y = 10;
+            int sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+
